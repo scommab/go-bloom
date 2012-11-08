262f15fdb21c98a44b115e1bb41fccd5ef0483f8
Saul <scommab@gmail.com>
2012-11-08 00:25:58 -0800

    ran go fmt

--- bloom.go
+++ bloom.go
@@ -1,80 +1,80 @@
-package bloom 
+package bloom
 
 import (
-  "crypto/sha1"
-  "encoding/binary"
-  "bytes"
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
 )
 
 const BITS_IN_BLOOM_TYPE = 8
 
 type BloomFilter struct {
-  bloom_filter []byte
-  size int
-  probe int
+	bloom_filter []byte
+	size         int
+	probe        int
 }
 
 func MakeBloomFilter(size int, probe int) *BloomFilter {
-  result := new(BloomFilter)
-  result.bloom_filter = make([]byte, size)
-  result.size = size
-  result.probe = probe 
-  return result
+	result := new(BloomFilter)
+	result.bloom_filter = make([]byte, size)
+	result.size = size
+	result.probe = probe
+	return result
 }
 
 func Sha1(data []byte) []byte {
-  var h = sha1.New()
-  h.Write(data)
-  return h.Sum(nil)
+	var h = sha1.New()
+	h.Write(data)
+	return h.Sum(nil)
 }
 
 func ToIndex(data []byte) int {
-  buf := bytes.NewBuffer(data)
-  result_64, err := binary.ReadVarint(buf)
-  result := int(result_64)
-  if err != nil {
-    panic(err)
-  }
-  if result < 0 {
-    result = -result
-  }
-  return result
+	buf := bytes.NewBuffer(data)
+	result_64, err := binary.ReadVarint(buf)
+	result := int(result_64)
+	if err != nil {
+		panic(err)
+	}
+	if result < 0 {
+		result = -result
+	}
+	return result
 }
 
 func (b *BloomFilter) SetBit(index int) {
-  index = index % (b.size * BITS_IN_BLOOM_TYPE)
-  loc := index / BITS_IN_BLOOM_TYPE
-  b.bloom_filter[loc] |= 1 << uint(index % BITS_IN_BLOOM_TYPE) 
+	index = index % (b.size * BITS_IN_BLOOM_TYPE)
+	loc := index / BITS_IN_BLOOM_TYPE
+	b.bloom_filter[loc] |= 1 << uint(index%BITS_IN_BLOOM_TYPE)
 }
 
 func (b *BloomFilter) IsBitSet(index int) bool {
-  index = index % (b.size * BITS_IN_BLOOM_TYPE)
-  loc := index / BITS_IN_BLOOM_TYPE
-  return (b.bloom_filter[loc] & byte(1 << uint(index % BITS_IN_BLOOM_TYPE))) != 0
+	index = index % (b.size * BITS_IN_BLOOM_TYPE)
+	loc := index / BITS_IN_BLOOM_TYPE
+	return (b.bloom_filter[loc] & byte(1<<uint(index%BITS_IN_BLOOM_TYPE))) != 0
 }
 
 func DataToBloomIndex(d []byte) ([]byte, int) {
-  var hash []byte = Sha1(d)
-  index := ToIndex(hash)
-  return hash, index
+	var hash []byte = Sha1(d)
+	index := ToIndex(hash)
+	return hash, index
 }
 
 func (b *BloomFilter) Add(d []byte) {
-  hash, output := DataToBloomIndex(d)
-  b.SetBit(output)
-  for i := 0; i < b.probe - 1; i++ {
-    hash, output = DataToBloomIndex(hash)
-    b.SetBit(output)
-  }
+	hash, output := DataToBloomIndex(d)
+	b.SetBit(output)
+	for i := 0; i < b.probe-1; i++ {
+		hash, output = DataToBloomIndex(hash)
+		b.SetBit(output)
+	}
 }
 
 func (b *BloomFilter) Has(d []byte) bool {
-  result := true
-  hash, output := DataToBloomIndex(d)
-  result = result && b.IsBitSet(output)
-  for i := 0; i < b.probe - 1; i++ {
-    hash, output = DataToBloomIndex(hash)
-    result = result && b.IsBitSet(output)
-  }
-  return result
+	result := true
+	hash, output := DataToBloomIndex(d)
+	result = result && b.IsBitSet(output)
+	for i := 0; i < b.probe-1; i++ {
+		hash, output = DataToBloomIndex(hash)
+		result = result && b.IsBitSet(output)
+	}
+	return result
 }
--- bloom_test.go
+++ bloom_test.go
@@ -1,65 +1,63 @@
 package bloom
 
 import (
-  "fmt"
-  "testing"
-  "os"
-  "bufio"
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func GetLines(file_name string) [][]byte {
-  var result [][]byte // empty array
+	var result [][]byte // empty array
 
-  f, err := os.Open(file_name)
-  if err != nil {
-    panic(err)
-  }
-  defer f.Close()
+	f, err := os.Open(file_name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-  r := bufio.NewReader(f)
-  line, _, err := r.ReadLine()
-  for err == nil {
-    // turn to a sting for easier printing
-    result = append(result, line)
-    line, _, err = r.ReadLine()
-  }
-  return result
+	r := bufio.NewReader(f)
+	line, _, err := r.ReadLine()
+	for err == nil {
+		// turn to a sting for easier printing
+		result = append(result, line)
+		line, _, err = r.ReadLine()
+	}
+	return result
 }
 
 func printHas(filter *BloomFilter, d []byte) bool {
-  has := filter.Has(d)
+	has := filter.Has(d)
 
-  fmt.Printf("%s", d)
-  if has {
-    fmt.Printf(" True\n")
-  } else {
-    fmt.Printf(" False\n")
-  }
-  return has
+	fmt.Printf("%s", d)
+	if has {
+		fmt.Printf(" True\n")
+	} else {
+		fmt.Printf(" False\n")
+	}
+	return has
 }
 
-func TestBloomTest(t *testing.T) {
-  var data = GetLines("data")
-  var test = GetLines("input")
-  filter := MakeBloomFilter(3, 2) //new(BloomFilter)
-  
-  for _, d := range data {
-    filter.Add(d)
-  }
- fmt.Printf("BLOOM-FILTER is %x\n", filter.bloom_filter)
+func TestBloom(t *testing.T) {
+	var data = GetLines("data")
+	var test = GetLines("input")
+	filter := MakeBloomFilter(3, 2) //new(BloomFilter)
 
+	for _, d := range data {
+		filter.Add(d)
+	}
+	fmt.Printf("BLOOM-FILTER is %x\n", filter.bloom_filter)
 
-  for _, d := range data {
-    if printHas(filter, d) != true {
-      t.Fatalf("Failed Match")
-    }
-  }
+	for _, d := range data {
+		if printHas(filter, d) != true {
+			t.Fatalf("Failed Match")
+		}
+	}
 
-  for _, d := range test {
-    if printHas(filter, d) != false {
-      t.Fatalf("Failed Match")
-    }
-  }
-
+	for _, d := range test {
+		if printHas(filter, d) != false {
+			t.Fatalf("Failed Match")
+		}
+	}
 
 }
