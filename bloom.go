bb2c1aa42433e1a4f6f5781009c2770338c1ed87
Saul <scommab@gmail.com>
2012-11-09 00:31:15 -0800

    Moved public func together and speed up Has

--- bloom.go
+++ bloom.go
@@ -14,17 +14,6 @@ type BloomFilter struct {
 	probe        int
 }
 
-// Create a new Bloom filter.
-// size - describes the number of bytes the Bloom filter.
-// probe - the number of probes each insert/lookup uses.
-func MakeBloomFilter(size uint, probe int) *BloomFilter {
-	result := new(BloomFilter)
-	result.bloom_filter = make([]uint64, size)
-	result.size = size
-	result.probe = probe
-	return result
-}
-
 func sha1_hash(data []byte) []byte {
 	var h = sha1.New()
 	h.Write(data)
@@ -43,19 +32,30 @@ func toIndex(data []byte) uint {
 func (b *BloomFilter) setBit(index uint) {
 	index = index % (b.size * BITS_IN_BLOOM_TYPE)
 	loc := index / BITS_IN_BLOOM_TYPE
-	b.bloom_filter[loc] |= 1 << uint(index % BITS_IN_BLOOM_TYPE)
+	b.bloom_filter[loc] |= 1 << uint(index%BITS_IN_BLOOM_TYPE)
 }
 
 func (b *BloomFilter) isBitSet(index uint) bool {
 	index = index % (b.size * BITS_IN_BLOOM_TYPE)
 	loc := index / BITS_IN_BLOOM_TYPE
-	return (b.bloom_filter[loc] & (1<<uint(index % BITS_IN_BLOOM_TYPE))) != 0
+	return (b.bloom_filter[loc] & (1 << uint(index%BITS_IN_BLOOM_TYPE))) != 0
 }
 
 func dataToBloomIndex(d []byte) ([]byte, uint) {
 	var hash []byte = sha1_hash(d)
 	index := toIndex(hash)
 	return hash, index
+}
+
+// Create a new Bloom filter.
+// size - the number of int64 to use for the Bloom filter.
+// probe - the number of probes each insert/lookup uses.
+func MakeBloomFilter(size uint, probe int) *BloomFilter {
+	result := new(BloomFilter)
+	result.bloom_filter = make([]uint64, size)
+	result.size = size
+	result.probe = probe
+	return result
 }
 
 // Add an item to the Bloom filter
@@ -72,12 +72,15 @@ func (b *BloomFilter) Add(d []byte) {
 // Returns False if the value definitely isn't in the Bloom filter.
 // Returns True if the value could be in the Bloom filter.
 func (b *BloomFilter) Has(d []byte) bool {
-	result := true
 	hash, output := dataToBloomIndex(d)
-	result = result && b.isBitSet(output)
+	if !b.isBitSet(output) {
+		return false
+	}
 	for i := 0; i < b.probe-1; i++ {
 		hash, output = dataToBloomIndex(hash)
-		result = result && b.isBitSet(output)
+		if !b.isBitSet(output) {
+			return false
+		}
 	}
-	return result
+	return true
 }
